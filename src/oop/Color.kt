864c95ed46ebf02efb8a9eb8170337797f15eaa3
Alezithop <alezithop@hotmail.com>
2021-04-20 15:49:15 -0500

    session 49 of Kotlin course completed

--- src/oop/Color.kt
+++ src/oop/Color.kt
@@ -1,5 +1,21 @@
 package oop
 
-enum class Color {
-    RED, BLUE, GREEN
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), BLUE(0x0000FF), GREEN(0x00FF00), YELLOW(0xFFFF00)
+}
+
+fun main(args: Array<String>) {
+    // 0..9 -> 2137 (decimal)
+    // 0..9, A, B, C, D, E, F -> 0x10
+    println(0x10) // 16
+    println(0x1F) // 31
+    println(0xFF) // 255
+    println(0xCAFEBABE)
+// https://www.w3schools.com/colors/colors_picker.asp
+    println(0x000000) // black
+    println(0xFFFFFF) // white
+    println(0xFF0000) // red
+    println(0x00FF00) // green
+    println(0x0000FF) // blue
+    println(0x000088) // dark blue
 }
