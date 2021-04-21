ecc22cfba997419cf9a0d756df6468a97e66c0de
Alezithop <alezithop@hotmail.com>
2021-04-21 11:41:29 -0500

    class 52 from Kotlin course

---
+++ src/oop/Animal.kt
@@ -0,0 +1,19 @@
+package oop
+
+class Animal {
+    var age: Int = 0
+    get() = field
+    set(value) {
+        if (value >= 0) {
+            field = value
+        }
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val animal = Animal()
+    animal.age = 8
+    animal.age = -2
+    println(animal.age)
+}
