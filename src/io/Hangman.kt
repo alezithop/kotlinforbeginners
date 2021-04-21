fa117b949e34449dfac4bfb0495093ab56fb1baf
Alezithop <alezithop@hotmail.com>
2021-04-21 14:02:36 -0500

    classes 60 and 61 from Kotlin course

---
+++ src/io/Hangman.kt
@@ -0,0 +1,63 @@
+package io
+
+fun main(args: Array<String>) {
+    print("Enter the word to guess: ")
+    val word = readLine()
+    if (word == null) {
+        println("No word given, game ended.")
+        return
+    }
+
+    for(i in 1..100) {
+        println()
+    }
+
+    // Tree
+    // tree
+    // ['t', 'r', 'e', 'e']
+    // {'t', 'r', 'e'}
+    val letters = word.toLowerCase().toCharArray().toHashSet()
+    val correctGuesses = mutableSetOf<Char>()
+    var fails = 0
+
+    while (letters != correctGuesses) {
+        // print the word that has been guessed so far
+        // _ _ _ _ _
+        // _ e _ e _
+        printExploredWord(word, correctGuesses)
+        println("\n#Wrong guesses: $fails \n\n")
+
+        print("Guess letter: ")
+        val input = readLine()
+
+        if (input == null) {
+            continue
+        } else if(input.length != 1) {
+            println("Please enter one letter")
+            continue
+        }
+
+        if (word.toLowerCase().contains(input.toLowerCase())) {
+            correctGuesses.add(input[0].toLowerCase())
+        } else {
+            fails++
+        }
+    }
+    printExploredWord(word, correctGuesses)
+    println("\n#Wrong guesses: $fails \n\n")
+    println("Well done")
+
+//    printExploredWord("Kotlin", mutableSetOf('o', 'l'))
+//    printExploredWord("Kotlin", mutableSetOf('k', 'o', 'l'))
+//    printExploredWord("Koltlin", mutableSetOf())
+}
+
+fun printExploredWord(word: String, correctGuesses: Set<Char>) {
+    for (character in word.toLowerCase()) {
+        if (correctGuesses.contains(character)) {
+            print(character + " ")
+        } else {
+            print("_ ")
+        }
+    }
+}
